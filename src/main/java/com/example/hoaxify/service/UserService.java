c7cb7cb688769f8ab9000871c99b29e1cb1eb618
Alexey <dignitionn@gmail.com>
2023-03-08 20:42:17 +0200

    change getUsers method to return Page

--- src/main/java/com/example/hoaxify/service/UserService.java
+++ src/main/java/com/example/hoaxify/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.hoaxify.service;
 
+import com.example.hoaxify.dto.UserDto;
 import com.example.hoaxify.persistence.entity.UserEntity;
 import com.example.hoaxify.persistence.entity.enums.Role;
 import com.example.hoaxify.persistence.entity.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +18,22 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final ModelMapper modelMapper;
+
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.modelMapper = modelMapper;
     }
 
     public UserEntity save(UserEntity userEntity) {
         userEntity.setRole(Role.USER);
         userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
+    }
+
+    public Page<UserDto> getUsers(Pageable page) {
+        return userRepository.findAll(page).map(user-> modelMapper.map(user, UserDto.class));
     }
 }
