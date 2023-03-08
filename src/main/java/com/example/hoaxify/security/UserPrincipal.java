9bf996a2baad217ac0ee4bc45bb12e15e9233ad4
Alexey <dignitionn@gmail.com>
2023-03-08 20:44:04 +0200

    removed @JsonIgnore since now we are returning UserDto

--- src/main/java/com/example/hoaxify/security/UserPrincipal.java
+++ src/main/java/com/example/hoaxify/security/UserPrincipal.java
@@ -2,8 +2,6 @@ package com.example.hoaxify.security;
 
 import com.example.hoaxify.persistence.entity.UserEntity;
 import com.example.hoaxify.persistence.entity.enums.Role;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,18 +16,15 @@ public class UserPrincipal implements UserDetails {
         this.user = user;
     }
 
-    public long getId() {
+    public Long getId() {
         return this.user.getId();
     }
-
     public String getImage() {
         return this.user.getImage();
     }
     public String getDisplayName() {
         return this.user.getDisplayName();
     }
-
-    @JsonIgnore
     public Role getRole() {
         return this.user.getRole();
     }
@@ -37,14 +32,13 @@ public class UserPrincipal implements UserDetails {
 
 
     @Override
-    @JsonIgnore
+
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.user.getRole()));
     }
 
     @Override
-    @JsonIgnore
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+
     public String getPassword() {
         return this.user.getPassword();
     }
@@ -53,22 +47,22 @@ public class UserPrincipal implements UserDetails {
     public String getUsername() {
         return this.user.getUsername();
     }
-    @JsonIgnore
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-    @JsonIgnore
+
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-    @JsonIgnore
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-    @JsonIgnore
+
     @Override
     public boolean isEnabled() {
         return true;
