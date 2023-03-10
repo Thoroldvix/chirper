1e686f9c00c29cc6e5833604f1597eeafae533bb
Alexey <dignitionn@gmail.com>
2023-03-10 16:05:42 +0200

    add createdAt field

--- src/main/java/com/example/hoaxify/persistence/entity/Hoax.java
+++ src/main/java/com/example/hoaxify/persistence/entity/Hoax.java
@@ -1,8 +1,20 @@
 package com.example.hoaxify.persistence.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
+@Entity
 public class Hoax {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String content;
+
+
+    private LocalDateTime createdAt;
 }
