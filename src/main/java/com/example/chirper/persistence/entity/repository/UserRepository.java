33a7a322c58f6ca9aa7fdce6a76aa209419ee32c
Alexey <dignitionn@gmail.com>
2023-03-13 01:10:26 +0200

    add repository annotation

--- src/main/java/com/example/chirper/persistence/entity/repository/UserRepository.java
+++ src/main/java/com/example/chirper/persistence/entity/repository/UserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @EntityGraph(attributePaths = {"posts"}, type = EntityGraph.EntityGraphType.LOAD)
     Optional<UserEntity> findByUsername(String username);
