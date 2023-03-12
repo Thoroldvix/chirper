0c379c1d4842134b636b31726227f6abe847bf18
Alexey <dignitionn@gmail.com>
2023-03-13 01:11:30 +0200

    removed redundant import

--- src/main/java/com/example/chirper/maper/UserMapper.java
+++ src/main/java/com/example/chirper/maper/UserMapper.java
@@ -3,7 +3,6 @@ package com.example.chirper.maper;
 import com.example.chirper.dto.UserDto;
 import com.example.chirper.persistence.entity.UserEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
